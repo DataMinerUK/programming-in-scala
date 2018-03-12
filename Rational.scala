98ce6ce0be8e96e394169ce098cfe40bca215183
Nicola Hughes <nihughes@thoughtworks.com>
2018-03-11 20:05:20 -0700

    Adding method but cannot access class parameter on object out of scope

--- Rational.scala
+++ Rational.scala
@@ -1,4 +1,9 @@
 class Rational(n: Int, d: Int) {
   require(d !=0)
+
   override def toString = n + "/" + d
+
+  def add(that: Rational): Rational = {
+    new Rational(n * that.d + that.n * d, d * that.d)
+  }
 }
