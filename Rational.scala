fc7e715cbe49096c533a970c9086bc56ddf38635
Nicola Hughes <nihughes@thoughtworks.com>
2018-03-12 19:23:35 -0700

    Normalizing using private field and method

--- Rational.scala
+++ Rational.scala
@@ -1,8 +1,9 @@
 class Rational(n: Int, d: Int) {
   require(d !=0)
 
-  val numer: Int = n
-  val denom: Int = d
+  private val g = gcd(n.abs, d.abs)
+  val numer: Int = n / g
+  val denom: Int = d / g
 
   def this(n: Int) = this(n, 1)
 
@@ -10,5 +11,9 @@ class Rational(n: Int, d: Int) {
 
   def add(that: Rational): Rational = {
     new Rational(numer * that.denom + that.numer * denom, denom * that.denom)
+  }
+
+  private def gcd(a: Int, b: Int): Int = {
+    if (b == 0) a else gcd(b, a % b)
   }
 }
